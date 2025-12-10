f67d177fec1d5f102bb37a427ec145f9922f2235
yzy0102 <1273160817@qq.com>
2025-12-10 12:51:45 +0800

    c

--- src/emcfsys/_version.py
+++ src/emcfsys/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.2.dev47+nogit.g03660ba65.d20251210'
-__version_tuple__ = version_tuple = (0, 0, 2, 'dev47', 'nogit.g03660ba65.d20251210')
+__version__ = version = '0.0.2.dev46+nogit.g23fa00b0c.d20251210'
+__version_tuple__ = version_tuple = (0, 0, 2, 'dev46', 'nogit.g23fa00b0c.d20251210')
 
-__commit_id__ = commit_id = 'g03660ba65'
+__commit_id__ = commit_id = 'g23fa00b0c'
