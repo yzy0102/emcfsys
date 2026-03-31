15a766380c6e336258687a685cb95576340968c6
yzy0102 <1273160817@qq.com>
2026-03-31 16:41:37 +0800

    fix bug

--- src/emcfsys/_version.py
+++ src/emcfsys/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.7.dev4+gf12fde7b3.d20260215'
-__version_tuple__ = version_tuple = (0, 0, 7, 'dev4', 'gf12fde7b3.d20260215')
+__version__ = version = '0.0.8'
+__version_tuple__ = version_tuple = (0, 0, 8)
 
-__commit_id__ = commit_id = 'gf12fde7b3'
+__commit_id__ = commit_id = None
